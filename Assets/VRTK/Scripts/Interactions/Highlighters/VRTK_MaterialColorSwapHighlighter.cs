d60e7941fdee1199048ae43b1ca983632c730b8f
ousttrue <ousttrue@gmail.com>
2019-01-20 15:31:58 +0900

    Fixed for Unity-2018.2

--- Assets/VRTK/Scripts/Interactions/Highlighters/VRTK_MaterialColorSwapHighlighter.cs
+++ Assets/VRTK/Scripts/Interactions/Highlighters/VRTK_MaterialColorSwapHighlighter.cs
@@ -143,7 +143,7 @@ namespace VRTK.Highlighters
 
                     if (resetMainTexture && material.HasProperty("_MainTex"))
                     {
-                        renderer.material.SetTexture("_MainTex", new Texture());
+                        renderer.material.SetTexture("_MainTex", new Texture2D(2, 2));
                     }
 
                     if (material.HasProperty("_Color"))
